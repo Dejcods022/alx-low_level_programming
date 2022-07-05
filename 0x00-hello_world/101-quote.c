4c32f51ba8b0d02eb548cccec2d9533b0b31d087
Dejcods022 <medlife004@gmail.com>
2022-07-05 13:58:46 +0200

    advaned tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+write(2, "and that piece of art is useful" - Dora Korpr, 2015-10-19\n");
+return (1);
+}
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
+ *   followed by a new lie, to the standard error.
+ *   Return: Always 0 (success)
+ */
