a7e547aeabfe5286288704bd1515585c2c1b708e
Dejcods022 <medlife004@gmail.com>
2022-07-05 19:37:00 +0200

    qoute task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpr, 2015-10-19\n");
+write(2, "and that piece of art is useful" - Dora Korpor, 2015 - 10 - 19\n");
 return (1);
 }
-/**
+/***
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19
  *   followed by a new lie, to the standard error.
