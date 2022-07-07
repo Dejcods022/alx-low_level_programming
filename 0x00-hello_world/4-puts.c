cd3e7a24cad8eb0ffc661ca17918be91254eb097
Dejcods022 <medlife004@gmail.com>
2022-07-07 10:55:18 +0200

    hello world corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main (void);
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
 {
-puts("programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
