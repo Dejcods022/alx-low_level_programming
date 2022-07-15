ad1f1d9f9e218d5f304f4c8c007ce2a203b49985
Dejcods022 <medlife004@gmail.com>
2022-07-15 14:51:52 +0200

    print more numbers more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *more_numbers -> prints 0-14 ten times
+ */
+void more_numbers(void)
+{
+
+	int x; y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <=14; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
+
