52c881ff9dd6abddb29ad63b01900d42909a0cfe
Dejcods022 <medlife004@gmail.com>
2022-07-15 11:41:21 +0200

    multilpy two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul -> multiply to integers
+ *@a: integer papram 1
+ *@b: integer param 2
+ *Return: succes and integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
