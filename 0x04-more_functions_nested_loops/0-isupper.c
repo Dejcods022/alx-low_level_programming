1e41bf8e66346dc229cfc0d290c2bcb15581ec12
Dejcods022 <medlife004@gmail.com>
2022-07-15 11:12:49 +0200

    check uppercase  in c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper -> is a function to check upper letter alphaebets
+ *@c: an integer parameter
+ *Return: something but success
+ */
+int _isupper(int c)
+{
+
+	return (c >= 'A' && c <= 'Z');
+}
