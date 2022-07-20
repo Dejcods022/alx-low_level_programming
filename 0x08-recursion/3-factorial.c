9a5a689ea0e859ac3df2ff7c96c937f4d050d568
Dejcods022 <medlife004@gmail.com>
2022-07-20 12:45:31 +0200

    define in main

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *factorial -> function prints factorial
+ *@n: integer param
+ *Return: an integer
+ */
+int factorial(int n);
+{
+	if (n < 0)
+		return (-1);
+	if (n = 0)
+		return (1);
