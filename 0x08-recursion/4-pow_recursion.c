caa344c85e1c2d6c64bb48195ff525230a09759b
Dejcods022 <medlife004@gmail.com>
2022-07-20 13:04:01 +0200

    power recursion duuh

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_pow_recursion -> power recursion
+ *@x:x^Y
+ *@y: the ecponent
+ *return: exponential number
+ */
+int _pow_recursion(int x, int y);
+{
+
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
