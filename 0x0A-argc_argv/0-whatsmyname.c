75afc9bcf66694ba26f6b4ea3f89095b86d3a02f
Dejcods022 <medlife004@gmail.com>
2022-07-26 19:55:33 +0200

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main -> a function to ptint its name
+ *@argv: an array of command list
+ *@argc: is an argc parameter
+ *Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
