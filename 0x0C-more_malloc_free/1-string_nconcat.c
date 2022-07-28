6f35129d34da3d3da51a91067a602a0da4bc0840
Dejcods022 <medlife004@gmail.com>
2022-07-28 13:58:52 +0200

    concat -something

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ *string_nconcat -> a function that concarenates two strings
+ *@n: an integer parameter
+ *@s1: a string parameter
+ *@s2: a string parameter
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+
+
+
+}
