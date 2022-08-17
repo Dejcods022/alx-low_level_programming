b7348057973d35c1d703ec57f933496c3c86867a
Dejcods022 <medlife004@gmail.com>
2022-08-17 20:47:35 +0200

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,10 @@
 /**
  *reset_to_98 -> a function that updates the reference of a pointer
  *@n: pointer to n to be updated
+ *Return: Always 0.
  */
-void reser_to_98(int *n)
+
+void reset_to_98(int *n)
 {
 
 	*n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN.H
 #define MAIN.H
-int _putchar(char c)
+int _putchar(char c);
+void reset_to_98(int *n);
 
 #endif
