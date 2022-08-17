4509abbc0c5a93b86d5dfdc15d8426682db73cc5
Dejcods022 <medlife004@gmail.com>
2022-08-17 22:53:11 +0200

    debugging assignment

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,9 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * positive_or_negative - tests if an integer is positive or negative
- * @i: an integer parameter
- * @n: an integer parameter
- * Retun: Always 0 (success)
- */
-
-void positive_or_negative(int i)
-{
-	int n;
-	int i;
-
-	srand(time(0));
-	n = i;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
