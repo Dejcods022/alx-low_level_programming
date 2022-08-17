042f0a2f1554dba5f1e441a1e3e3e2880329c917
Dejcods022 <medlife004@gmail.com>
2022-08-17 21:56:44 +0200

    string legth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen -> functoin to get the length of the string
+ *@s: a string poiter
+ *Return: the length of the string
+ */
+int _strlen(char *s)
+{
+
+	return (strlen(s));
+}
